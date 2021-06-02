0b220a76345ed11b933fe72872e03bce4a4f60f9
Senayson <senaialema@gmail.com>
2021-06-02 22:56:48 +0000

    created input.js for module to export to play.js"

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 const net = require("net");
 const { stderr } = require("process");
 const {connect} = require('./client');
-
-const handleUserInput = function (data) {
-  //console.log(data);
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const {setupInput} = require('./input');
 
 
 
-setupInput();
 console.log("Connecting ...");
 connect();
+setupInput();

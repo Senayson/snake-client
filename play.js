ebddf4945c773366acc5eb557c40f4da87bac304
Senayson <senaialema@gmail.com>
2021-06-02 22:48:52 +0000

    Handled user input with stdin

--- play.js
+++ play.js
@@ -1,7 +1,24 @@
 const net = require("net");
+const { stderr } = require("process");
 const {connect} = require('./client');
 
+const handleUserInput = function (data) {
+  //console.log(data);
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
 
+
+setupInput();
 console.log("Connecting ...");
 connect();
